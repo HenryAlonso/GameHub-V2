2ec7d2d2409f33bd1d00524ec6d7e6d22ce597f0
Henry Alonso <henryalonso78@gmail.com>
2023-07-20 19:52:12 -0400

    First commit

--- flask_app/models/game.py
+++ flask_app/models/game.py
@@ -1,8 +1,9 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import user
 from flask import flash
 
 class Game:
-    db = 'gamehub_db'
+    DB = "arbotrary_pybelt"
 
     def __init__(self, data):
         self.id = data['id']
@@ -14,108 +15,112 @@ class Game:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         self.user_id = data['user_id']
+        self.creator = None
 
     @classmethod
-    def create_game(cls, data):
-        query = '''
-        INSERT INTO games (title, platform, genre, release_date, description, created_at, user_id)
-        VALUES (%(title)s, %(platform)s, %(genre)s, %(release_date)s, %(description)s, NOW(), %(user_id)s);
-        '''
-        return connectToMySQL(cls.db).query_db(query, data)
-
-    @classmethod
-    def validate_game(cls, data):
-        is_valid = True
-        if int(len(data['species'])) <= 0 or int(len(data['location'])) <= 0:
-            is_valid = False
-            flash('All fields required')
-            return is_valid
-        if len(data['species']) <3:
-            flash('Species must be at least 3 characters')
-            is_valid = False
-        if len(data['location']) <3:
-            flash('Location must be at least 3 characters')
-            is_valid = False
-        if int(len(data['reason'])) <=0:
-            flash('Reason must not be blank')
-            is_valid = False
-        if not data['date']:
-            flash('Date must not be blank')
-            is_valid = False
-        return is_valid
-
-    @classmethod
-    def get_all(cls):
-        query = '''
-        SELECT * FROM games
-        LEFT JOIN users ON games.user_id = users.id;
-        '''
-        results = connectToMySQL(cls.db).query_db(query)
+    def retrieve_all(cls):
+        query = """
+                SELECT * FROM games
+                LEFT JOIN users
+                ON games.user_id = users.id
+        """
+        results = connectToMySQL(cls.DB).query_db(query)
         games = []
         for row in results:
+            game = cls(row)
             user_data = {
-                'id': row['user_id'],
-                'first_name': row['first_name'],
-                'last_name': row['last_name'],
-                'email': row['email'],
-                'password': row['password'],
-                'created_at': row['created_at'],
-                'updated_at': row['updated_at']
+                "id": row['users.id'],
+                "first_name": row['first_name'],
+                "last_name": row['last_name'],
+                "username" : row['username'],
+                "email": row['email'],
+                "password": "",
+                "created_at": row['users.created_at'],
+                "updated_at": row['users.updated_at']
             }
-            game = {
-                'id': row['id'],
-                'species': row['species'],
-                'location': row['location'],
-                'reason': row['reason'],
-                'date': row['date'],
-                'created_at': row['created_at'],
-                'updated_at': row['updated_at'],
-                'user_id': row['user_id'],
-                'user': user_data
-            }
+            game.creator = user.User(user_data)
             games.append(game)
         return games
-
+        
     @classmethod
-    def get_one_game(cls, data):
-        query = '''
-        SELECT * FROM games
-        LEFT JOIN users ON games.user_id = users.id
-        WHERE games.id = %(id)s;
-        '''
-        results = connectToMySQL(cls.db).query_db(query, data)
-        for row in results:
-            user_data = {
-                'id': row['user_id'],
-                'first_name': row['first_name'],
-                'last_name': row['last_name'],
-                'email': row['email'],
-                'password': row['password'],
-                'created_at': row['users.created_at'],
-                'updated_at': row['users.updated_at']
-            }
-            game = {
-                'id': row['id'],
-                'species': row['species'],
-                'location': row['location'],
-                'reason': row['reason'],
-                'date': row['date'],
-                'created_at': row['created_at'],
-                'updated_at': row['updated_at'],
-                'user_id': row['user_id'],
-                'user': user_data
-            }
-        return game
-
+    def retrieve_by_id(cls, data):
+        query = """
+                SELECT * from games
+                JOIN users
+                ON games.user_id = users.id
+                WHERE games.id = %(id)s;
+        """
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        if not results:
+            return False
+        result = results[0]
+        this_game = cls(result)
+        user_data = {
+            "id": result['users.id'],
+            "first_name": result['first_name'],
+            "last_name": result['last_name'],
+            "username": result['username'],
+            "email": result['email'],
+            "password": "",
+            "created_at": result['users.created_at'],
+            "updated_at": result['users.updated_at']
+        }
+        this_game.creator = user.User(user_data)
+        return this_game
+    
     @classmethod
-    def update_game(cls, data):
-        query = '''
-        UPDATE games SET species = %(species)s, location = %(location)s, reason = %(reason)s, date = %(date)s, updated_at = NOW()
-        WHERE id = %(id)s;
-        '''
-        return connectToMySQL(cls.db).query_db(query, data)
-
+    def new_game(cls, data):
+        query = """
+                INSERT INTO games 
+                ( title, platform, genre, release_date, description, created_at, user_id )
+                VALUES
+                ( %(title)s, %(platform)s, %(genre)s, %(release_date)s, %(description)s, NOW(), %(user_id)s )
+        """
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        print(results)
+        return results
+    
     @classmethod
-    def delete(cls, data):
-        query = 'DELETE FROM games WHERE id = %(id)s;'
-        return connectToMySQL(cls.db).query_db(query, data)
+    def re_plant(cls, data):
+        query = """
+                UPDATE games
+                # SET title = %(title)s, platform = %(platform)s, genre = %(genre)s, release_date = %(release_date)s, description = %(description)s, updated_at = NOW()
+                WHERE id = %(id)s;
+        """
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        print(results)
+        return results
+    
+    @classmethod
+    def remove_game(cls, data):
+        query = """
+                DELETE FROM games
+                WHERE id = %(id)s;
+        """
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        print("##########################")
+        print(results)
+        return results
+    
+    @staticmethod
+    def validate_game_form(games):
+        is_valid = True
+        if not games.get('title') or not games.get('platform') or not games.get('genre') or not games.get('release_date') or not games.get('description'):
+            flash("All fields required.", "games")
+            is_valid = False
+        if len(games['title']) < 1:
+            flash("Title must contain at least 1 characters.", "games")
+            is_valid = False
+        if len(games['platform']) < 2:
+            flash("Platform must contain at least 2 characters.", "games")
+            is_valid = False
+        if len(games['genre']) < 3:
+            flash("Genre must be at least 3 characters.", "games")
+            is_valid = False
+        if not games['release_date']:
+            flash("Release date is required. Please enter a date.", "games")
+            is_valid = False
+        if len(games['description']) < 20:
+            flash("Description must be at least 20 characters.", "games")
+            is_valid = False
+        return is_valid
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,128 +1,147 @@
 from flask_app.config.mysqlconnection import connectToMySQL
-from flask import flash
+from flask_app.models import game
 import re
-EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from flask import flash
+from flask_app import bcrypt
+
+EMAIL_REGEX =  re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class User:
-    db = 'gamehub_db'
+    DB = "arbotrary_pybelt"
 
     def __init__(self, data):
         self.id = data['id']
         self.first_name = data['first_name']
-        self.last_name = data['last_name']
+        self.last_name= data['last_name']
+        self.username = data['username']
         self.email = data['email']
         self.password = data['password']
         self.birthday = data['birthday']
         self.description = data['description']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-        self.games = [];
-
-    @staticmethod
-    def validate_user(cls, data):
-        is_valid = True
-        query = 'SELECT * FROM users WHERE email = %(email)s;'
-        results = connectToMySQL(cls.db).query_db(query, data)
-        if int(len(data['first_name'])) <= 0 or int(len(data['last_name'])) <= 0 or int(len(data['email'])) <= 0 or int(len(data['password'])) <= 0:
-            is_valid = False
-            flash('All fields required', 'register')
-            return is_valid
-        if int(len(results)) >= 1:
-            flash('Email already in use', 'register')
-            is_valid = False
-        if not EMAIL_REGEX.match(data['email']):
-            flash('Invalid Email format', 'register')
-            is_valid = False
-        if int(len(data['first_name'])) < 3:
-            flash('First name must be at least 3 characters', 'register')
-            is_valid = False
-        if int(len(data['last_name'])) < 3:
-            flash('Last name must be at least 3 characters', 'register')
-            is_valid = False
-        if int(len(data['password'])) < 8:
-            flash('Password must be at least 8 characters', 'register')
-            is_valid = False
-        if data['password'] != data['confirm']:
-            flash('Passwords must match', 'register')
-            is_valid = False
-        return is_valid
+        self.games = []
 
     @classmethod
-    def create_user(cls, data):
-        query = '''
-        INSERT INTO users (first_name, last_name, email, password)
-        VALUES ( %(first_name)s, %(last_name)s, %(email)s, %(password)s );
-        '''
-        return connectToMySQL(cls.db).query_db(query, data)
+    def add_user(cls, data):
+        query = """
+                INSERT into users ( first_name, last_name, username, email, password )
+                VALUES ( %(first_name)s, %(last_name)s, %(username)s, %(email)s, %(password)s );
+        """
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        print(results)
+        return results
+    
+    @classmethod
+    def get_all(cls):
+        query = """
+                SELECT * FROM users;
+        """
+        results = connectToMySQL(cls.DB).query_db(query)
+        users = []
+        for user in results:
+            users.append( cls(user))
+        return users
 
     @classmethod
-    def get_one(cls, data):
-        query = 'SELECT * FROM users WHERE id = %(id)s;'
-        results = connectToMySQL(cls.db).query_db(query,data)
-        if results:
-            return cls(results[0])
-
+    def get_user_by_id(cls, data):
+        query = """
+                SELECT * FROM users WHERE id = %(id)s;
+        """
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        print(results)
+        return cls(results[0])
+    
     @classmethod
-    def user_games(cls, data):
-        query = '''
-        SELECT * FROM users
-        LEFT JOIN games ON games.user_id = users.id
-        WHERE users.id = %(id)s;
-        '''
-        results = connectToMySQL(cls.db).query_db(query, data)
-        user = cls(results[0])
-        for row in results:
-            user_data = {
-                'id': row['user_id'],
-                'first_name': row['first_name'],
-                'last_name': row['last_name'],
-                'email': row['email'],
-                'password': row['password'],
-                'created_at': row['created_at'],
-                'updated_at': row['updated_at']
-            }
-            game_data = {
-                'id': row['id'],
-                'title': row['title'],
-                'platform': row['platform'],
-                'genre': row['genre'],
-                'release_date': row['release_date'],
-                'description': row['description'],
-                'created_at': row['created_at'],
-                'updated_at': row['updated_at'],
-                'user_id': row['user_id'],
-                'user': user_data
-            }
-            # user.games.append( game.Game(game_data) )
-        return user
-    #     @classmethod
-    # def get_user_with_trees(cls, data):
-    #     query = """
-    #             SELECT * FROM users
-    #             LEFT JOIN trees
-    #             ON users.id = trees.user_id
-    #             WHERE users.id = %(id)s;
-    #     """
-    #     results = connectToMySQL(cls.DB).query_db(query, data)
-    #     user = cls(results[0])
-    #     for row in results:
-    #         tree_data = {
-    #             "id": row['trees.id'],
-    #             "species": row['species'],
-    #             "location": row['location'],
-    #             "reason": row['reason'],
-    #             "date_planted": row['date_planted'],
-    #             "created_at": row['trees.created_at'],
-    #             "updated_at": row['trees.updated_at'],
-    #             "user_id" : row['user_id']
-    #         }
-    #         user.trees.append( tree.Tree(tree_data))
-    #     return user
-
-    @classmethod
-    def check_email(cls, data):
-        query = 'SELECT * FROM users WHERE email = %(email)s;'
-        results = connectToMySQL(cls.db).query_db(query, data)
+    def get_user_by_email(cls, data):
+        query = """
+                SELECT * FROM users WHERE email = %(email)s;
+        """
+        results = connectToMySQL(cls.DB).query_db(query, data)
         if len(results) < 1:
             return False
         return cls(results[0])
+    
+    @classmethod
+    def get_user_with_games(cls, data):
+        query = """
+                SELECT * FROM users
+                LEFT JOIN games
+                ON users.id = games.user_id
+                WHERE users.id = %(id)s;
+        """
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        user = cls(results[0])
+        for row in results:
+            game_data = {
+                "id": row['games.id'],
+                "title": row['games.title'],
+                "platform": row['games.platform'],
+                "genre": row['games.genre'],
+                "release_date": row['games.release_date'],
+                "description": row['games.description'],
+                "created_at": row['games.created_at'],
+                "updated_at": row['games.updated_at'],
+                "user_id" : row['user_id']
+            }
+            user.games.append( game.Game(game_data))
+        return user
+
+    
+    @staticmethod
+    def validate_user_form(user):
+        is_valid = True
+        if not user.get('email') or not user.get('first_name') or not user.get('last_name') or not user.get('username') or not user.get('password') or not user.get('confirm'):
+            flash("All fields are required.", "register")
+            is_valid = False
+        else:
+            query = "SELECT * FROM users WHERE email = %(email)s;"
+            results = connectToMySQL(User.DB).query_db(query, user)
+            if len(results) >= 1:
+                flash("Email already taken", "register")
+                is_valid = False
+            if not EMAIL_REGEX.match(user['email']):
+                flash("Invalid email address.", "register")
+                is_valid = False
+            if not user['first_name'].isalpha():
+                flash("First name must contain only alphabetic letters.", "register")
+                is_valid = False
+            elif len(user['first_name']) < 2:
+                flash("First name must be at least 2 characters.", "register")
+                is_valid = False
+            if len(user['username']) < 3:
+                flash("Username must be at least 3 characters.", "register")
+                is_valid = False
+            if not user['last_name'].isalpha():
+                flash("Last name must contain only alphabetic letters.", "register")
+                is_valid = False
+            elif len(user['last_name']) < 2:
+                flash("Last name must be at least 2 characters.", "register")
+                is_valid = False
+            if len(user['password']) < 8:
+                flash("Password must contain at least 8 characters", "register")
+                is_valid = False
+            elif not re.search(r"\d", user['password']):
+                flash("Password must contain at least one number", "register")
+                is_valid = False
+            elif not re.search(r"[A-Z]", user['password']):
+                flash("Password must contain at least one uppercase letter", "register")
+                is_valid = False
+            if user['password'] != user['confirm']:
+                flash("Passwords do not match", "register")
+                is_valid = False
+        return is_valid
+    
+    @staticmethod
+    def validate_user_login(user_form):
+        if not EMAIL_REGEX.match(user_form['email']):
+            flash("Invalid email/password.", "login")
+            return False
+        user = User.get_user_by_email(user_form)
+        if not user:
+            flash("Invalid email/password", "login")
+            return False
+        if not bcrypt.check_password_hash(user.password, user_form['password']):
+            flash("Invalid email/password", "login")
+            return False
+        return user
